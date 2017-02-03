120694daae31910d19fc5d2c6ded34dc83676f4e
cboehmke <cboehmke@DESKTOP-J02ETF3>
2017-02-03 14:48:28 -0600

    Add project files.

---
+++ Small Project/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Small_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.txtSaveAs = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(479, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "inputFile";
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fileLabel.Location = new System.Drawing.Point(125, 43);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(278, 29);
+            this.fileLabel.TabIndex = 1;
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(42, 49);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 2;
+            this.openFileButton.Text = "&Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Location = new System.Drawing.Point(125, 123);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(278, 29);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(39, 132);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(78, 13);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Saving File To:";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(125, 87);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(328, 87);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // txtSaveAs
+            // 
+            this.txtSaveAs.Location = new System.Drawing.Point(409, 132);
+            this.txtSaveAs.Name = "txtSaveAs";
+            this.txtSaveAs.Size = new System.Drawing.Size(43, 20);
+            this.txtSaveAs.TabIndex = 7;
+            this.txtSaveAs.Text = "...";
+            this.txtSaveAs.UseVisualStyleBackColor = true;
+            this.txtSaveAs.Click += new System.EventHandler(this.txtSaveAs_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.startButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 186);
+            this.Controls.Add(this.txtSaveAs);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Small Project";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button txtSaveAs;
+    }
+}
+
---
+++ Small Project/Form1.cs
@@ -0,0 +1,603 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Small_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Mill function
+        public string ConvertMill(string a)
+        {
+            switch (a)
+            {
+                case "AP":
+                    a = "Appleton Coated";
+                    break;
+
+                case "AV":
+                    a = "Appvion";
+                    break;
+
+                case "BP":
+                    a = "Boise Pacific";
+                    break;
+
+                case "CT":
+                    a = "Catalyst";
+                    break;
+
+                case "CV":
+                    a = "CVG";
+                    break;
+
+                case "DO":
+                    a = "Domtar";
+                    break;
+
+                case "EG":
+                    a = "Enterprise Group";
+                    break;
+
+                case "FS":
+                    a = "Felix Schoeller";
+                    break;
+
+                case "FI":
+                    a = "Finch";
+                    break;
+
+                case "GP":
+                    a = "Georgia Pacific";
+                    break;
+
+                case "GF":
+                    a = "Glatfelter";
+                    break;
+
+                case "IP":
+                    a = "International Paper";
+                    break;
+
+                case "MC":
+                    a = "Mac Paper";
+                    break;
+
+                case "MA":
+                    a = "Marquee";
+                    break;
+
+                case "MD":
+                    a = "Midland";
+                    break;
+
+                case "MI":
+                    a = "Mitsubishi";
+                    break;
+
+                case "MO":
+                    a = "Mondi";
+                    break;
+
+                case "NE":
+                    a = "Neenah";
+                    break;
+
+                case "NO":
+                    a = "Norpac";
+                    break;
+
+                case "PP":
+                    a = "Performance Papers";
+                    break;
+
+                case "PL":
+                    a = "Permalite";
+                    break;
+
+                case "RE":
+                    a = "Resolute";
+                    break;
+
+                case "RO":
+                    a = "Roland";
+                    break;
+
+                case "VE":
+                    a = "Verso";
+                    break;
+
+                case "CU":
+                    a = "Custom";
+                    break;
+                    
+
+            }
+            return a;
+        }
+
+        // Weight Type function
+        public string ConvertWeight(string a)
+        {
+            switch (a)
+            {
+                case "T":
+                    a = "lb Text";
+                    break;
+
+                case "B":
+                    a = "lb Bond";
+                    break;
+
+                case "C":
+                    a = "lb Cover";
+                    break;
+
+                case "I":
+                    a = "lb Index";
+                    break;
+
+                case "P":
+                    a = "Point";
+                    break;       
+
+            }
+            return a;
+        }
+
+        // Finish function
+        public string ConvertFinish(string a)
+        {
+            switch(a)
+            {
+                case "IJT":
+                    a = "Inkject Treated";
+                    break;
+
+                case "CDU":
+                    a = "Coated Dull";
+                    break;
+
+                case "CGL":
+                    a = "Coated Gloss";
+                    break;
+
+                case "CMA":
+                    a = "Coated Matte";
+                    break;
+
+                case "CSA":
+                    a = "Coated Satin";
+                    break;
+
+                case "CSI":
+                    a = "Coated Silk";
+                    break;
+
+                case "CVV":
+                    a = "Coated Velvet";
+                    break;
+
+                case "UNC":
+                    a = "Uncoated";
+                    break;
+            }
+            return a;
+        }
+
+        // Ink Type function
+        public string ConvertInk(string a)
+        {
+            switch(a)
+            {
+                case "1":
+                    a = "IV1 / MO5";
+                    break;
+
+                case "2":
+                    a = "IV2 / MO6";
+                    break;
+            }
+            return a;
+        }
+
+        // Color Grip function
+        public string ConvertColorGrip(string a)
+        {
+            switch(a)
+            {
+                case "C":
+                    a = "Enabled";
+                    break;
+
+                case "N":
+                    a = "Disabled";
+                    break;                     
+            }
+            return a;
+        }
+
+        // TAC Level function
+        public string ConvertTAC(string a)
+        {
+            switch(a)
+            {
+                case "A":
+                    a = "50";
+                    break;
+
+                case "B":
+                    a = "60";
+                    break;
+
+                case "C":
+                    a = "70";
+                    break;
+
+                case "D":
+                    a = "80";
+                    break;
+
+                case "E":
+                    a = "90";
+                    break;
+
+                case "F":
+                    a = "100";
+                    break;
+
+                case "G":
+                    a = "110";
+                    break;
+
+                case "H":
+                    a = "120";
+                    break;
+
+                case "I":
+                    a = "130";
+                    break;
+
+                case "J":
+                    a = "140";
+                    break;
+
+                case "K":
+                    a = "150";
+                    break;
+
+                case "L":
+                    a = "160";
+                    break;
+
+                case "M":
+                    a = "170";
+                    break;
+
+                case "N":
+                    a = "180";
+                    break;
+
+                case "O":
+                    a = "190";
+                    break;
+
+                case "P":
+                    a = "200";
+                    break;             
+            }
+            return a;
+        }
+
+        // Color Grip Level function
+        public string ConvertColorGripLevel(string a)
+        {
+            switch(a)
+            {
+                case "A":
+                    a = "5";
+                    break;
+
+                case "B":
+                    a = "10";
+                    break;
+
+                case "C":
+                    a = "15";
+                    break;
+
+                case "D":
+                    a = "20";
+                    break;
+
+                case "E":
+                    a = "25";
+                    break;
+
+                case "F":
+                    a = "30";
+                    break;
+
+                case "G":
+                    a = "40";
+                    break;
+
+                case "H":
+                    a = "50";
+                    break;
+
+                case "0":
+                    a = "0";
+                    break;
+            }
+            return a;
+        }
+
+        // Format Description function
+        public string FormatDescription(string a)
+        {
+            StringBuilder description = new StringBuilder(a.Length * 2);
+            description.Append(a[0]);
+            for (int i = 1; i < a.Length; i++)
+            {
+                // add space before capital letters unless a capital precedes it (acronyms)
+                if (char.IsUpper(a[i]))
+                {
+                    if (char.IsUpper(a[i - 1]) && char.IsUpper(a[i + 1]))
+                    {
+                        description.Append("");
+                    }
+                    
+                    else if (char.IsLower(a[i - 1]))
+                    {
+                        description.Append(' ');
+                    }
+
+                }
+
+
+                // add space before numbers, unless a number precedes it
+                if (char.IsDigit(a[i]))
+                {
+
+                    if (char.IsDigit(a[i - 1]))
+                    {
+                        description.Append("");
+                    }
+                    else if (char.IsLower(a[i - 1]))
+                    {
+                        description.Append(' ');
+                    }
+
+                    else if (char.IsUpper(a[i - 1]))
+                    {
+                        description.Append(' ');
+                    }
+                }
+                // fix stubborn ones
+                switch (description.ToString())
+                {
+                    case "Earth Choice 30Recycle":
+                        description.Replace("Earth Choice 30Recycle", "Earth Choice 30 Recycle");
+                        break;
+
+                    case "Spectrum Premium 92Br":
+                        description.Replace("Spectrum Premium 92Br", "Spectrum Premium 92 Br");
+                        break;
+
+                    case "Spectrum WIJColor Pro":
+                        description.Replace("Spectrum WIJColor Pro", "Spectrum WIJ Color Pro");
+                        break;
+
+                    case "SSMHAccent Image Lok":
+                        description.Replace("SSMHAccent Image Lok", "SSMH Accent Image Lok");
+                        break;
+
+                    case "Swordi JET 4_3Gloss":
+                        description.Replace("Swordi JET 4_3Gloss", "Swordi JET 4_3 Gloss");
+                        break;
+
+                    case "DNSHigh Speed Coated Feel":
+                        description.Replace("DNSHigh Speed Coated Feel", "DNS High Speed Coated Feel");
+                        break;
+
+                    case "DNSHigh Speed IJCF":
+                        description.Replace("DNSHigh Speed IJCF", "DNS High Speed IJCF");
+                        break;
+
+                    case "Kromos 95Uncoated":
+                        description.Replace("Kromos 95Uncoated", "Kromos 95 Uncoated");
+                        break;
+
+                    case "ORCACPMultipurpose":
+                        description.Replace("ORCACPMultipurpose", "ORCACP Multipurpose");
+                        break;
+
+                    case "DNSHigh Speed IJNF":
+                        description.Replace("DNSHigh Speed IJNF", "DNS High Speed IJNF");
+                        break;
+
+                    case "IQSelection Smooth":
+                        description.Replace("IQSelection Smooth", "IQ Selection Smooth");
+                        break;
+
+                }
+
+
+
+                // append line
+                description.Append(a[i]);
+
+            }
+            return description.ToString();
+        }
+
+                
+
+        // input validation variables
+        public bool fileSelected = false;
+        public bool saveAsSelected = false;
+         
+        // Open File
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                
+                // allow user to select input file and add it to label
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    fileLabel.Text = openFileDialog1.FileName;
+                    fileSelected = true;
+                }
+
+            }
+
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Start
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (fileSelected == true && saveAsSelected == true)
+            {
+
+                try
+                {
+
+                    string[] line = File.ReadAllLines(fileLabel.Text);
+
+                    StringBuilder csv = new StringBuilder();
+                    csv.Append("FileName,Mill,Description,PaperWeight,WeightType,GSMWeight,Finish,InkVersion,ColorGrip,TACLevel,ColorGripLevel" + Environment.NewLine);
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        var temp = line[i].Replace(".zip", "");
+
+                        // Filename
+                        csv.Append(line[i] + ",");
+
+                        // Mill 2 chars
+                        csv.Append(ConvertMill(temp.Substring(0, 2)) + ",");
+
+                        // Description 1-20 chars
+                        csv.Append(FormatDescription(temp.Substring(2, (temp.Length - 16)) + ","));
+
+                        // PaperWeight 3 digits
+                        csv.Append(temp.Substring(temp.Length - 14, 3) + ",");
+
+                        // WeightType 1 char
+                        csv.Append(ConvertWeight(temp.Substring(temp.Length - 11, 1)) + ",");
+
+                        // GSMWeight 3 digits
+                        csv.Append(temp.Substring(temp.Length - 10, 3) + ",");
+
+                        // Finish 3 chars
+                        csv.Append(ConvertFinish(temp.Substring(temp.Length - 7, 3)) + ",");
+
+                        // InkVersion 1 char
+                        csv.Append(ConvertInk(temp.Substring(temp.Length - 4, 1)) + ",");
+
+                        // ColorGrip Option 1 char
+                        csv.Append(ConvertColorGrip(temp.Substring(temp.Length - 3, 1)) + ",");
+
+                        // TAC Level 1 char
+                        csv.Append(ConvertTAC(temp.Substring(temp.Length - 2, 1)) + ",");
+
+                        // ColorGripLevel 1 char
+                        csv.Append(ConvertColorGripLevel(temp.Substring(temp.Length - 1, 1)) + ",");
+
+                        // Reserved 4 chars
+                        //csv += temp.Substring(35, 4) + ",";
+
+
+                        csv.Append(Environment.NewLine);
+
+                    }
+
+                    // output contents to file
+
+                    var output = outputLabel.Text;
+                    if (!output.EndsWith(".csv"))
+                    {
+                        output = output + ".csv";
+                    }
+
+                    StreamWriter file = new StreamWriter(output);
+
+                    file.WriteLine(csv);
+
+                    file.Close();
+
+                    MessageBox.Show("Contents successfully transferred to CSV file.");
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must select both an input file and a 'Save As' location before beginning.");
+            }
+        }
+
+            
+        // Clear labels
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            fileLabel.Text = "";
+            outputLabel.Text = "";
+        }
+
+        // Save As
+        private void txtSaveAs_Click(object sender, EventArgs e)
+        {
+           
+            // let user select file's 'save as' location
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFileDialog1.DefaultExt = "csv";
+            saveFile.ShowDialog();
+            outputLabel.Text = saveFile.FileName;
+            saveAsSelected = true;
+            
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+        
